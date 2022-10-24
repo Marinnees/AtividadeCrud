bbc69b8dfbbcda1d87201261041766431fc199d8
Marinnees <108021547+Marinnees@users.noreply.github.com>
2022-10-24 20:21:46 -0300

    Subindo nova versão Site/Spring com MVC

---
+++ MeuSiteSpringBoot/src/main/java/Model/Usuario.java
@@ -0,0 +1,63 @@
+package Model;
+
+public class Usuario {
+	// atributos
+	private int id_usuario;
+	private String nome;
+	private String email;
+	private String senha;
+	private String endereco;
+
+	// Contrutor
+	public Usuario(String nome, String email, String senha, String endereco) {
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.endereco = endereco;
+	}
+
+	public Usuario() {
+	}
+
+	//Gets e Sets
+	public int getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+}
---
+++ MeuSiteSpringBoot/src/main/java/viagens/aqui/MeuSiteSpringBootApplication.java
@@ -0,0 +1,13 @@
+package viagens.aqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuSiteSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuSiteSpringBootApplication.class, args);
+	}
+
+}
